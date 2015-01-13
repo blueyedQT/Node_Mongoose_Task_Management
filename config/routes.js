9c9f49b54c5a58761009b3bbe934784c8afeecd3
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-13 08:46:19 -0800

    Basic html skeleton for table and form

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
 	User = mongoose.model('User');
 module.exports = function Routes(app){
 	app.get('/', function(req, res){
-		res.render('index', {title: 'hello'});
+		res.render('index', {title: 'Task Management'});
 	});
 	app.post('/users/create', function(req, res){
 		var a = new User(req.body);
